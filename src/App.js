bfd44ca190bf06fd02065b7479890e74df4bbf65
miku86 <git@miku86.com>
2021-10-27 18:08:50 +0200

    Lint

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import * as React from "react";
-import { ethers } from "ethers";
 import './App.css';
 
 export default function App() {
@@ -13,7 +12,7 @@ export default function App() {
 
       <div className="dataContainer">
         <div className="header">
-        👋 Hey there!
+        Hey there!
         </div>
 
         <div className="bio">
